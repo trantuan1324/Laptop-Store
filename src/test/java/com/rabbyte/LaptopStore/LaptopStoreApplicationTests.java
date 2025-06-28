1b19f3b4c884f43acfaf3afb14cf0cde0f41dc0f
Rabbyte <quangtuantran1324@gmail.com>
2025-06-29 02:08:24 +0700

    config association between Order-User and Role-User

---
+++ src/test/java/com/rabbyte/LaptopStore/LaptopStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbyte.LaptopStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
